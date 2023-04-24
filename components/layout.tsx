1c38f4b18e7bc3ac3d4b9ae26b4979052fa7cd97
AI-65 <ilja.garber@googlemail.com>
2023-04-24 12:05:15 +0200

    best layout version so far

--- components/layout.tsx
+++ components/layout.tsx
@@ -24,6 +24,14 @@ export default function Layout({ children }: LayoutProps) {
         <footer className="w-full bottom-0 z-40 bg-black bg-opacity-50">
           <div className="container h-16 border-t border-t-slate-200 py-4 flex flex-col justify-center items-center">
             <h2 className="text-green-400 font-mono text-sm">Demo built by Ilja Garber</h2>
+            <a
+              className="text-green-400 font-mono text-sm hover:underline"
+              href="https://github.com/AI-65"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @AI-65
+            </a>
           </div>
         </footer>
       </div>
